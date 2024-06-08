e513ec4e8dbf2cca250a78af363b6b40f66a9796
aman-kejriwal <84925133+aman-kejriwal@users.noreply.github.com>
2024-06-08 15:14:00 +0530

    async Component in Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,21 @@ import Image from "next/image";
 async function getUSerData(){
   const res=await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
 
-  return res;
+  return res.data;
 } 
 export default async function Home(){
   const userDetails=await getUSerData();
   return (
-    <div>
-      Hi There!
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userDetails?.name}
+                </div>
+                
+                {userDetails?.email}
+            </div>
+        </div>
     </div>
   )
 }
