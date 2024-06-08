37c4aefb24019d9d0dcd1af40fd60e8b01b84317
aman-kejriwal <84925133+aman-kejriwal@users.noreply.github.com>
2024-06-09 03:00:34 +0530

    slight modification

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,7 +3,7 @@ import {PrismaClient} from "@prisma/client"
 const client=new PrismaClient();
 export async function POST(req: NextRequest) {
     const body = await req.json();
-    await client.userser.create({
+    await client.user.create({
         data:{
             username:body.username,
             password:body.password
