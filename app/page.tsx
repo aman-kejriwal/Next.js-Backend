9aa83a512ae97fa7072f7ef7157a66e6f7dd507b
aman-kejriwal <84925133+aman-kejriwal@users.noreply.github.com>
2024-06-08 21:19:50 +0530

    final Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,10 @@
+import UserDetails from "@/components/UserDetails";
 import axios from "axios";
 import Image from "next/image";
 
-async function getUSerData(){
-  await new Promise((r)=>setTimeout(r,5000));
-  const res=await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
 
-  return res.data;
-} 
 export default async function Home(){
-  
-  const userDetails=await getUSerData();
   return (
-    <div className="flex flex-col justify-center h-screen">
-        <div className="flex justify-center">
-            <div className="border p-8 rounded">
-                <div>
-                    Name: {userDetails?.name}
-                </div>
-                
-                Email: {userDetails?.email}
-            </div>
-        </div>
-    </div>
+    <UserDetails/>
   )
 }
