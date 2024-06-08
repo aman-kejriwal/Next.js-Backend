57bd5355e38b4fa51bdf368de9b5388326a95d16
aman-kejriwal <84925133+aman-kejriwal@users.noreply.github.com>
2024-06-09 02:49:53 +0530

    SignUp using PostgreSql db

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
-
+import {PrismaClient} from "@prisma/client"
+const client=new PrismaClient();
 export async function POST(req: NextRequest) {
     const body = await req.json();
+    await client.userser.create({
+        data:{
+            username:body.username,
+            password:body.password
+        }
+    })
     console.log(body);
     return Response.json({
         msg:"you are Logged in"
