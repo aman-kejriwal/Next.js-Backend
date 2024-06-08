77ca003fbf1f6c08bdec65096aface36db04503c
aman-kejriwal <84925133+aman-kejriwal@users.noreply.github.com>
2024-06-08 19:01:10 +0530

    Added anime loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,12 @@
-   export default function(){
+import React from 'react';
+
+export default function Loading() {
     return (
-        <div>
-            Loading....
+        <div className="flex flex-col justify-center h-screen">
+            <div className="flex justify-center">
+              <div style={{ width: "100px", height: "100px", backgroundImage: "url('https://superstorefinder.net/support/wp-content/uploads/2018/01/4colors.gif')", backgroundSize: "cover" }}>
+              </div>
+            </div>
         </div>
-    )
-   }
+    );
+}
