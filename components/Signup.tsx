2241f574af01eefe87a64a26b65ab7ebdc2e1169
aman-kejriwal <84925133+aman-kejriwal@users.noreply.github.com>
2024-06-09 14:09:54 +0530

    added navigationWhen when loggedIn

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -1,12 +1,12 @@
 "use client"
 import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export function Signup() {
     const [username, setUsername] = useState("fhadfadf");
     const [password, setPassword] = useState("adfhdf");
-    // const router=useRouter();
+    const router=useRouter();
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
@@ -29,7 +29,7 @@ export function Signup() {
                                 password
                             }
                             );
-                            // router.push("/");
+                            router.push("/");
                         }} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
                 </div>
